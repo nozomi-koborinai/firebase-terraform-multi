b39354d62c2bd64949fcb411fdc4ac1043a23de1
nozomi-koborinai <kobofender@gmail.com>
2023-10-14 00:59:23 +0900

    add: google provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    },
     google-beta = {
       source  = "hashicorp/google-beta"
       version = "~> 5.0"
