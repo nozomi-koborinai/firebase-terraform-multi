20aac6b3eae6f1931ad8e52192a20c9cd87007b0
nozomi-koborinai <kobofender@gmail.com>
2023-10-14 01:00:57 +0900

    fix

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
-    },
+    }
     google-beta = {
       source  = "hashicorp/google-beta"
       version = "~> 5.0"
