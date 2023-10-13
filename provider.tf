9d4f5b63fd60364fbbdced7f6a30a0453c5c1811
nozomi-koborinai <kobofender@gmail.com>
2023-10-14 01:02:10 +0900

    remove: google-provider

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
     google-beta = {
       source  = "hashicorp/google-beta"
       version = "~> 5.0"
